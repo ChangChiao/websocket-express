d8047f57b663e1322a2df144e9a23aed0fd4c829
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-06 14:18:50 +0800

    feat: add ping event

--- websocket.js
+++ websocket.js
@@ -1,14 +1,22 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8082 });
 
 wss.on("connection", function connection(ws) {
+  console.log("connection");
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    messages.push(data.toString());
-    console.log(messages);
+    // messages.push(data.toString());
+    console.log(data);
   });
 
-  ws.send(messages);
+  ws.on("close", function close() {
+    console.log("disconnected");
+  });
+
+  setInterval(() => {
+    ws.send("ping" + new Date().getTime());
+  }, 3000);
+  //   ws.send(messages);
 });
