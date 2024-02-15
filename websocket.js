03aeadb44017d10ebc2ac3c5717f47d31bfa1e43
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-15 14:05:19 +0800

    refactor: JSON stringify send data

--- websocket.js
+++ websocket.js
@@ -8,15 +8,22 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", function message(data) {
     // messages.push(data.toString());
-    console.log(data);
+    console.log("server-received==", data);
+    // if (data === "ping") {
+    ws.send(JSON.stringify({ message: "ping" }));
+    // }
   });
+  //   const pingInterval = setInterval(() => {
+  //     ws.send("ping");
+  //     // ws.ping();
+  //   }, 3000);
 
   ws.on("close", function close() {
+    // clearInterval(pingInterval);
     console.log("disconnected");
   });
 
-  setInterval(() => {
-    ws.send("ping" + new Date().getTime());
-  }, 3000);
-  //   ws.send(messages);
+  //   ws.on("pong", () => {
+  //     console.log("pong received");
+  //   });
 });
